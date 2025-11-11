f9d261e16a12504d362625ffa0fb03d7f67175ce
DeYwid <dpustula1314@gmail.com>
2025-11-11 21:31:23 +0100

    asdasadas

---
+++ Programowanie/PracticalTaskConsoleApp/2023_01/Notatka.cs
@@ -0,0 +1,41 @@
+﻿namespace PracticalTaskConsoleApp.Styczeń2023_1
+{
+    /**********************************************
+    Klasa: Notatka
+    Opis: Reprezentuje prosty system do tworzenia notatek,
+          które posiadają unikalne ID, tytuł oraz treść.
+    Pola:
+      - id: prywatne, przechowuje identyfikator notatki (int)
+      - tytul: chronione, przechowuje tytuł notatki (string)
+      - tresc: chronione, przechowuje treść notatki (string)
+      - licznikNotatek: prywatne statyczne, zlicza utworzone notatki (int)
+    Autor: Daniel Pustuła
+    ***********************************************/
+
+    internal class Notatka
+    {
+        private static int licznikNotatek = 0;
+        private int id;
+        protected string tytul;
+        protected string tresc;
+
+        public Notatka(string tytul, string tresc)
+        {
+            licznikNotatek++;
+            id = licznikNotatek;
+            this.tytul = tytul;
+            this.tresc = tresc;
+        }
+
+        public void WyswietlNotatke()
+        {
+            Console.WriteLine($"Tytuł: {tytul}");
+            Console.WriteLine($"Treść: {tresc}");
+        }
+
+        public void Diagnostyczna()
+        {
+            Console.WriteLine($"[Diagnostyka] ID: {id}, Tytuł: {tytul}, Treść: {tresc}");
+        }
+    }
+}
--- Programowanie/PracticalTaskConsoleApp/Program.cs
+++ Programowanie/PracticalTaskConsoleApp/Program.cs
@@ -2,18 +2,25 @@
 using PracticalTaskConsoleApp.Czerwiec2023_6;
 using PracticalTaskConsoleApp.Styczeń2023_1;
 using PracticalTaskConsoleApp.Czerwiec2022_2;
-Task1 task1 = new Task1();
+using PracticalTaskConsoleApp.Styczeń2023_1;
+
+    Task2 task1 = new Task2();
 task1.Task();
 
 Console.WriteLine("\n");
+
 
 Task2 task2 = new Task2();
 task2.Task();
 
 Console.WriteLine("\n");
 
-Task3 task3 = new Task3();
+Task4 task3 = new Task4();
 task3.Task();
+
+Task4 task4 = new Task4();
+task4.Task();
 
 Task5 task5 = new Task5();
 task5.Task();
+
