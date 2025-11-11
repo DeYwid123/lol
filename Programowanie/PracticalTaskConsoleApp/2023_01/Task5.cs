6a4136eeae0ba42761c014794bd234789b397815
DeYwid <dpustula1314@gmail.com>
2025-11-11 21:31:23 +0100

    asdasadas

---
+++ Programowanie/PracticalTaskConsoleApp/2023_01/Task5.cs
@@ -0,0 +1,19 @@
+﻿namespace PracticalTaskConsoleApp.Styczeń2023_1
+{
+    internal class Task2
+    {
+        public void Task()
+        {
+            Notatka pierwszaNotatka = new Notatka("Zakupy", "Kupić mleko, chleb i jajka.");
+            Notatka drugaNotatka = new Notatka("Praca", "Zakończyć raport do piątku.");
+
+            Console.WriteLine("=== Pierwsza notatka ===");
+            pierwszaNotatka.WyswietlNotatke();
+            pierwszaNotatka.Diagnostyczna();
+
+            Console.WriteLine("\n=== Druga notatka ===");
+            drugaNotatka.WyswietlNotatke();
+            drugaNotatka.Diagnostyczna();
+        }
+    }
+}
